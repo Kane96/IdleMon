c9d2b390a4122ba88950df4182ab5985fb859118
KaneCr96 <k.crichton96@gmail.com>
2019-04-01 20:55:26 +0100

    Added basic Pokemon structure and battle structure.

---
+++ Assets/Combat/Team.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    public GameObject[] team = new GameObject[5];
+
+    public Pokemon GetPokemon(int slot) {
+        return team[slot].GetComponent<Pokemon>();
+    }
+}
---
+++ Assets/Pokemon/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon : MonoBehaviour
+{
+    public Sprite spriteFront;
+    public Sprite spriteBack;
+
+    public Sprite getSpriteFront() {
+        return spriteFront;
+    }
+
+    public Sprite getSpriteBack() {
+        return spriteBack;
+    }
+}
